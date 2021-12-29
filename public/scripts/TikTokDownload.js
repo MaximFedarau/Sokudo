fbaed5854090404ea500219045c064c733404e0c
maxim <fedarau@gmail.com>
2021-12-29 19:10:06 +0300

    Пофиксил костыль с месторасположением+добавил возможность менять месторасположение сохранения файла и добавил GUI к этому всему делу.

--- public/scripts/TikTokDownload.js
+++ public/scripts/TikTokDownload.js
@@ -3,80 +3,71 @@
 let tiktokDownloadButton = document.getElementById("tiktokDownloadButton")
 let tiktokLinkInput = document.getElementById("tiktokLinkInput")
 
+const ipcRendererTikTok =  window.require('electron').ipcRenderer;
+let usersFileStatusTikTok = true
+let usersFilePathTikTok = ''
+function getUsersDir() {
+    ipcRendererTikTok.send("createDialog")
+    ipcRendererTikTok.on("usersDir", (event,arg) => {
+        usersFileStatusTikTok = arg.canceled
+        usersFilePathTikTok = arg.filePaths[0]
+        console.log(usersFileStatusTikTok,usersFilePathTikTok)
+    })
+}
+let chooseDirTikTokButton = document.getElementById("chooseDirTiktok")
+chooseDirTikTokButton.onclick = function() {
+    getUsersDir()
+}
 
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
 let usersUrl = ""
 tiktokDownloadButton.onclick = function() {
 
     //возможно child_process
-
-    const { execFile } = require('child_process');
-    const {exec}  = require('child_process')
-    process.env.PATH = process.env.PATH + ':/usr/local/bin';
-    const child = exec(`node /Users/maxim/Desktop/webProjects/Sokudo/dist/mac-arm64/Sokudo.app/Contents/Resources/app/public/scripts/testScritpt.js ${tiktokLinkInput.value}`,
-        (error, stdout, stderr) => {
-            console.log(`stdout: ${stdout}`);
-            usersUrl = stdout
-            console.log(usersUrl)
-            console.log(`stderr: ${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
-    const child1 = exec("node --version",
-        (error, stdout, stderr) => {
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });
-    const urlDownloader = require("url-downloader")
-    const resources  = [{
-        "target": "LOCAL",
-        "origin": usersUrl,
-        "destination": "/Users/maxim/Desktop/funnymeme.mp4"
+    if (usersFileStatusTikTok === true) {
+        alert("\t Some error occurred!\n Please, choose your directory!")
     }
-    ]
+    else {
+        const {execFile} = require('child_process');
+        const {exec} = require('child_process')
+        let ttData = [`${usersFilePathTikTok}/${makeid(20)}.mp4` + "~" + tiktokLinkInput.value]
+        process.env.PATH = process.env.PATH + ':/usr/local/bin';
+        const child = exec(`node ${__dirname + "/scripts/noWMTikTokScript.js"} ${ttData} `,
+            (error, stdout, stderr) => {
+                console.log(`stdout: ${stdout}`);
+                usersUrl = stdout
+                console.log(usersUrl)
+                console.log(`stderr: ${stderr}`);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);//прописать через __dirname
+                    alert("Some error occurred")
+                }
+                if (error === null) {
+                    alert("Finished downloading")
+                }
+            });
 
-    console.log(1)
-    const options = {
-        maxConcurrency: 30
+
+        tiktokLinkInput.value = ""
     }
-
-    urlDownloader.download(resources, options).then((results) => {
-        console.log('files downloaded and saved');
-        console.log(results);
-    })
-
-    tiktokLinkInput.value = ""
-
 
 
 }
 
-let testButton = document.getElementById("preferencesTiktok")
 
-testButton.onclick = function() {
-    console.log(usersUrl)
-    const urlDownloader = require("url-downloader")
-    const resources  = [{
-        "target": "LOCAL",
-        "origin": usersUrl,
-        "destination": "/Users/maxim/Desktop/funnymeme.mp4"
-    }
-    ]
+let usersPreferencesTikTokButton = document.getElementById("preferencesTiktok")
 
-    console.log(1)
-    const options = {
-        maxConcurrency: 30
-    }
-
-    urlDownloader.download(resources, options).then((results) => {
-        console.log('files downloaded and saved');
-        console.log(results);
-    })
-
-    tiktokLinkInput.value = ""
+usersPreferencesTikTokButton.onclick = function() {
+    alert("IN DEVELOPMENT STAGE")
 }
 /*const tiktok = require("tiktok-scraper-without-watermark")
 const url = "https://www.tiktok.com/@lilhancha/video/7042666995887738117?is_copy_url=1&is_from_webapp=v1"
--- public/scripts/testScritpt.js
+++
@@ -1,6 +0,0 @@
-const tiktok = require("tiktok-scraper-without-watermark")
-//const url = "https://www.tiktok.com/@kjxvier/video/7043992701116763397?is_copy_url=1&is_from_webapp=v1"
-
-let args = process.argv.slice(2);
-
-tiktok.tiktokdownload(args[0]).then(result => console.log(result.nowm))
